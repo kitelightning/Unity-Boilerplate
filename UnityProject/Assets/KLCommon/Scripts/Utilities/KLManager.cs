241ec48fb1786c091add938cab5a2e98f76fe25a
ikrima <contact@ikrima.com>
2014-03-10 02:14:30 -0700

    Adding KL Manager scripts

---
+++ UnityProject/Assets/KLCommon/Scripts/Utilities/KLManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KLManager : MonoBehaviour {
+
+#if (DEBUG || UNITY_EDITOR)
+    bool displayConsole = false;
+
+    // Use this for initialization
+    void Start () {
+    
+    }
+    
+    // Update is called once per frame
+    void Update () {
+        if (Input.GetKeyUp(KeyCode.BackQuote))
+        {
+            displayConsole = !displayConsole;
+            DebugConsole.IsOpen = displayConsole;
+        }
+    }
+#endif
+
+}
